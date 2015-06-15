39365a93c5aa1a88564611167bf2bf8f2d1373fe
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-06-15 12:14:57 +0000

    add some test

--- sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/EmailTest.java
+++ sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/EmailTest.java
@@ -9,16 +9,23 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.startsWith;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,7 +41,28 @@ public class EmailTest {
     @Test
     public void testRichEmail() {
         assertThat(mActivityRule.getActivity(), is(notNullValue()));
-        onView(withId(R.id.view)).perform(typeText("myemail@soprasteria.com"));
+
+        // write invalid email
+        onView(withId(R.id.view)).perform(typeText("wrong format"));
+
+        // click validate button
+        onView(withId(R.id.validateButton)).perform(click());
+
+        // check error
+        onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.view))))
+                .check(matches(withText("42 /!\\ invalide email value")));
+
+        // write valid email
+        onView(allOf(withId(R.id.component_internal), isDescendantOfA(withId(R.id.view))))
+                .perform(clearText());
+        onView(withId(R.id.view)).perform(typeText("contact@gmail.com"));
+
+        // click validate button
+        onView(withId(R.id.validateButton)).perform(click());
+
+        // check no error
+        onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.view))))
+                .check(matches(withText(isEmptyOrNullString())));
     }
 
     @Test
