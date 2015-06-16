5daf21e0d4e147eb4e961d724c76d329b4f1641c
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-06-16 13:33:21 +0000

    more tests + add screenshot action

--- sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/RichEmailTest.java
+++ sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/RichEmailTest.java
@@ -4,6 +4,8 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import com.soprasteria.movalysmdk.widget.test.actions.SpoonScreenshotAction;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +18,15 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.notNullValue;
+
+import static com.soprasteria.movalysmdk.widget.test.actions.OrientationChangeAction.orientationLandscape;
+import static com.soprasteria.movalysmdk.widget.test.actions.OrientationChangeAction.orientationPortrait;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -40,6 +46,18 @@ public class RichEmailTest {
         onView(withId(R.id.validateButton)).perform(click());
 
         // check error
+        onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.view))))
+                .check(matches(withText("42 /!\\ invalide email value")));
+
+        SpoonScreenshotAction.perform("rightemail_invalidemail_errorstate");
+
+        onView(isRoot()).perform(orientationLandscape());
+
+        onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.view))))
+                .check(matches(withText("42 /!\\ invalide email value")));
+
+        onView(isRoot()).perform(orientationPortrait());
+
         onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.view))))
                 .check(matches(withText("42 /!\\ invalide email value")));
     }
