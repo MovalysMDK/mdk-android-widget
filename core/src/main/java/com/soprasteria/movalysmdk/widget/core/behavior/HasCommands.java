20c80c440310fba38d55a231ae605e04f58e019c
Guillaume Guimberteau <guillaume.guimberteau@soprasteria.com>
2015-06-22 09:59:57 +0000

    SONAR: feedback

--- core/src/main/java/com/soprasteria/movalysmdk/widget/core/behavior/HasCommands.java
+++ core/src/main/java/com/soprasteria/movalysmdk/widget/core/behavior/HasCommands.java
@@ -5,11 +5,11 @@ import android.view.View;
 /**
  * Interface to add button handling capacity to a widget.
  */
-public interface HasActions extends View.OnClickListener {
+public interface HasCommands extends View.OnClickListener {
 
 
     /**
-     * Register actions views on the components
+     * Register commands views on the components
      * (or its ActionDelegate).
      */
     void registerWidgetCommands();
