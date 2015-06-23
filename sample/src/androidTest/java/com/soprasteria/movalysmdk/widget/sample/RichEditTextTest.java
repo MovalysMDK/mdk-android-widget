b9467e2afd23682b3695e6abec27278978598d7e
Guillaume Guimberteau <guillaume.guimberteau@soprasteria.com>
2015-06-23 08:32:53 +0000

    Update test with softkeyboard managment. Detection of padding problem with scrollTo method.

--- sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/RichEditTextTest.java
+++ sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/RichEditTextTest.java
@@ -1,15 +1,37 @@
 package com.soprasteria.movalysmdk.widget.sample;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.CloseKeyboardAction;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.GeneralLocation;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
+import com.soprasteria.movalysmdk.widget.standard.MDKRichEditText;
+import com.soprasteria.movalysmdk.widget.test.actions.CustomScrollToAction;
+
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeoutException;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
@@ -33,9 +55,12 @@ public class RichEditTextTest {
 
         assertThat(mActivityRule.getActivity(), is(notNullValue()));
 
+        onView(withId(is(R.id.validateButton))).perform(scrollTo());
+
         // write text into RichEditText component
         onView(allOf(withId(R.id.component_internal), isDescendantOfA(withId(R.id.mdkRichEditText_withCustomLayout))))
-                .perform(typeText("Input text hides hint and label show down"));
+                .perform(typeText("Input text hides hint and label show down"), closeSoftKeyboard());
+
 
         // click on "Remplir" button to automatically fill out the RichEditText component with "Hello"
         onView(withId(R.id.richedittext_button_remplir_effacer)).perform(click());
@@ -55,20 +80,59 @@ public class RichEditTextTest {
         onView(withId(R.id.validateButton)).perform(click());
 
 
-       // onView(withId(R.id.test_case_1)).check(matches(withText("42 /!\\ Must de filled")));
+
         onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.mdkRichEditText_withLabelAndMandatory))))
                .check(matches(withText("42 /!\\ Must be filled")));
 
         // Write message into MDKRichEditText component
         onView(allOf(withId(R.id.component_internal), isDescendantOfA(withId(R.id.mdkRichEditText_withLabelAndMandatory))))
-                .perform(typeText("Input text"));
+                .perform(typeText("Input text"), closeSoftKeyboard());
 
-        // Perform click on validation button
-        onView(withId(R.id.validateButton)).perform(click());
+        //onView(allOf(withId(R.id.component_internal), isDescendantOfA(withId(R.id.mdkRichEditText_withCustomLayoutAndButton)))).perform(scrollTo());
+
+        // TODO remove CustomcrollToAction if there is no paddind in xml file
+        // Use CustomScrollToAction to managa problem with Espresso (it does not take account of padding during scrollTo action).
+        onView(withId(R.id.validateButton)).perform(ViewActions.actionWithAssertions(new CustomScrollToAction()), click());
 
         // Check that the MDKEditText component raises no error after validating it
         onView(allOf(withId(R.id.component_error), isDescendantOfA(withId(R.id.mdkRichEditText_withLabelAndMandatory))))
               .check(matches(withText(R.string.empty_string)));
 
+    }
+
+
+    /**
+     * Custom closeSoftKeyboard method.
+     * Workaround to manage time delay to close softkeyboard bug.
+     * @return ViewAction the view action
+     */
+    public static ViewAction closeSoftKeyboard() {
+        return new ViewAction() {
+            /**
+             * The delay time to allow the soft keyboard to dismiss.
+             */
+            private static final long KEYBOARD_DISMISSAL_DELAY_MILLIS = 1000L;
+
+            /**
+             * The real {@link CloseKeyboardAction} instance.
+             */
+            private final ViewAction mCloseSoftKeyboard = new CloseKeyboardAction();
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return mCloseSoftKeyboard.getConstraints();
+            }
+
+            @Override
+            public String getDescription() {
+                return mCloseSoftKeyboard.getDescription();
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                mCloseSoftKeyboard.perform(uiController, view);
+                uiController.loopMainThreadForAtLeast(KEYBOARD_DISMISSAL_DELAY_MILLIS);
+            }
+        };
     }
 }
