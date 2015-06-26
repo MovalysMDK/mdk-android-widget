f633c4afebf7ddaaf162b67cf5cb7cdd012c48dc
Guillaume Guimberteau <guillaume.guimberteau@soprasteria.com>
2015-06-26 13:49:44 +0000

    Update EmailTest with mandatory button test.

--- sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/EmailTest.java
+++ sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/EmailTest.java
@@ -18,33 +18,21 @@ package com.soprasteria.movalysmdk.widget.sample;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.widget.Button;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.action.ViewActions.pressBack;
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.startsWith;
-import static org.hamcrest.Matchers.endsWith;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -105,5 +93,21 @@ public class EmailTest {
 
         // Check widgets are enabled
         onView(withId(R.id.mdkEmail_withErrorAndCommandOutside)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void testMandatoryEmail() {
+        assertThat(mActivityRule.getActivity(), is(notNullValue()));
+
+        // Check widgets are mandatory
+        onView(withId(R.id.mdkEmail_withErrorAndCommandOutside))
+                .check(matches(withHint("Test hint (*)")));
+
+        // remove mandatory option on widget
+        onView(withId(R.id.mandatoryButton)).perform(click());
+
+        // Check widgets are no more mandatory
+        onView(withId(R.id.mdkEmail_withErrorAndCommandOutside))
+                .check(matches(withHint("Test hint")));
     }
 }
