ab7e61a947dd08726c63eb3dcbd49030d9b526b3
Antoine Belliard <antoine.belliard@soprasteria.com>
2015-06-30 14:11:49 +0000

    Add HasDate interface behavior

---
+++ core/src/main/java/com/soprasteria/movalysmdk/widget/core/behavior/HasDate.java
@@ -0,0 +1,13 @@
+package com.soprasteria.movalysmdk.widget.core.behavior;
+
+import java.util.Date;
+
+/**
+ * Add date behavior on a widget.
+ */
+public interface HasDate {
+
+    Date getDate();
+
+    void setDate(Date date);
+}
