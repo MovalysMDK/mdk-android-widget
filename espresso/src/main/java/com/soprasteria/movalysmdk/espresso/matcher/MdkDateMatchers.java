bacd85ea511e27ac75db8cd4c71e822285e37638
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-07-17 09:12:15 +0000

    refactoring espresso

---
+++ espresso/src/main/java/com/soprasteria/movalysmdk/espresso/matcher/MdkDateMatchers.java
@@ -0,0 +1,107 @@
+package com.soprasteria.movalysmdk.espresso.matcher;
+
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.view.View;
+
+import com.soprasteria.movalysmdk.widget.core.MDKBaseRichDateWidget;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Add view matchers on date for espresso tests.
+ */
+public class MdkDateMatchers {
+
+    /**
+     * Constructor.
+     */
+    private MdkDateMatchers() {
+        // empty constructor
+    }
+
+    /**
+     * Create a matcher to check a date.
+     * @param year year.
+     * @param month month.
+     * @param day day.
+     * @return matcher.
+     */
+    public static Matcher<View> withDate( final int year, final int month, final int day) {
+        return matchDateTime(year, month, day, 0, 0);
+    }
+
+    /**
+     * Create a matcher to check a date with time.
+     * @param year year.
+     * @param month month.
+     * @param day day.
+     * @param hour hour.
+     * @param minute minute.
+     * @return matcher.
+     */
+    public static Matcher<View> withDateTime( final int year, final int month, final int day, final int hour, final int minute) {
+        return matchDateTime(year, month, day, hour, minute);
+    }
+
+    /**
+     * Create a matcher to check a date.
+     * @param year year.
+     * @param month month.
+     * @param day day of month.
+     * @param hour hour.
+     * @param minute minute.
+     * @return matcher.
+     */
+    private static Matcher<View> matchDateTime( final int year, final int month, final int day, final int hour, final int minute ) {
+        return new BoundedMatcher(MDKBaseRichDateWidget.class) {
+
+            /**
+             * Expected date.
+             */
+            private Calendar expectedDate ;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with date: ");
+                description.appendText(Integer.toString(year));
+                description.appendText("/");
+                description.appendText(Integer.toString(month));
+                description.appendText("/");
+                description.appendText(Integer.toString(day));
+                description.appendText(" ");
+                description.appendText(Integer.toString(hour));
+                description.appendText(":");
+                description.appendText(Integer.toString(minute));
+
+                if(null != this.expectedDate) {
+                    description.appendText(" value: ");
+                    description.appendText(this.expectedDate.toString());
+                }
+
+            }
+
+            @Override
+            public boolean matchesSafely(Object object) {
+                MDKBaseRichDateWidget dateView = (MDKBaseRichDateWidget) object;
+                if(null == this.expectedDate) {
+                    this.expectedDate = Calendar.getInstance();
+                    expectedDate.set(Calendar.YEAR, year);
+                    expectedDate.set(Calendar.MONTH, month-1);
+                    expectedDate.set(Calendar.DAY_OF_MONTH, day);
+                    expectedDate.set(Calendar.HOUR_OF_DAY, hour);
+                    expectedDate.set(Calendar.MINUTE, minute);
+                    expectedDate.set(Calendar.SECOND, 0);
+                    expectedDate.set(Calendar.MILLISECOND, 0);
+                }
+
+                Date actualDate = dateView.getDate();
+
+                return null != this.expectedDate && null != actualDate?this.expectedDate.getTime().equals(actualDate):false;
+            }
+        };
+    }
+}
---
+++ espresso/src/main/java/com/soprasteria/movalysmdk/espresso/matcher/MdkLabelMatchers.java
@@ -0,0 +1,158 @@
+package com.soprasteria.movalysmdk.espresso.matcher;
+
+import android.content.res.Resources;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Espresso matchers for checking labels.
+ */
+public class MdkLabelMatchers {
+
+    /**
+     * Log tag.
+     */
+    private static final String LOG_TAG = "MdkLabelMatchers";
+
+    /**
+     * Create a matcher to check the label in the text of a view.
+     * <p>If the view is mandatory, the mandatory string (*) is added to the string comparison.</p>
+     * @param labelId label id.
+     * @return matcher.
+     */
+    public static Matcher<View> withTextLabel(@StringRes final int labelId) {
+        return withLabel(labelId, null, LABEL_TEXT);
+    }
+
+    /**
+     * Create a matcher to check the label in the text of a view.
+     * <p>If the view is mandatory, the mandatory string (*) is added to the string comparison.</p>
+     * @param labelId label id.
+     * @param mandatoryChar mandatory symbol if field is mandatory.
+     * @return matcher.
+     */
+    public static Matcher<View> withTextLabel(@StringRes final int labelId, @StringRes final int mandatoryChar) {
+        return withLabel(labelId, mandatoryChar, LABEL_TEXT);
+    }
+
+    /**
+     * Create a matcher to check the label in the hint of a view.
+     * <p>If the view is mandatory, the mandatory string (*) is added to the string comparison.</p>
+     * @param labelId label id.
+     * @return matcher.
+     */
+    public static Matcher<View> withHintLabel(@StringRes final int labelId ) {
+        return withLabel(labelId, null, LABEL_HINT);
+    }
+
+    /**
+     * Create a matcher to check the label in the hint of a view.
+     * <p>If the view is mandatory, the mandatory string (*) is added to the string comparison.</p>
+     * @param labelId label id.
+     * @param mandatoryChar mandatory symbol if field is mandatory.
+     * @return matcher.
+     */
+    public static Matcher<View> withHintLabel(@StringRes final int labelId, @StringRes final int mandatoryChar) {
+        return withLabel(labelId, mandatoryChar, LABEL_HINT);
+    }
+
+    /**
+     * Create a matcher to check the label.
+     * <p>If the view is mandatory, the mandatory string (*) is added to the string comparison.</p>
+     * @param labelId label id.
+     * @param mandatoryChar mandatory symbol if field is mandatory.
+     * @param labelLocation location of label (in text or hint)
+     * @return matcher.
+     */
+    private static Matcher<View> withLabel(@StringRes final int labelId, @Nullable @StringRes final Integer mandatoryChar, @LabelLocation final int labelLocation) {
+        return new BoundedMatcher(TextView.class) {
+
+            /**
+             * Resource names.
+             */
+            private String resourceName ;
+
+            /**
+             * Concat text.
+             */
+            private String expectedText ;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with string from label id: ");
+                description.appendText(Integer.toString(labelId));
+
+                if(null != this.resourceName) {
+                    description.appendText("[");
+                    description.appendText(this.resourceName);
+                    description.appendText(", mandatoryChar:");
+                    description.appendText(Integer.toString(mandatoryChar));
+                    description.appendText("]");
+                }
+
+                if(null != this.expectedText) {
+                    description.appendText(" value: ");
+                    description.appendText(this.expectedText);
+                }
+
+            }
+
+            @Override
+            public boolean matchesSafely(Object object) {
+                TextView textView = (TextView) object;
+                if(null == this.expectedText) {
+                    try {
+                        StringBuilder text = new StringBuilder(textView.getResources().getString(labelId));
+                        if ( mandatoryChar != null) {
+                            text.append(textView.getResources().getString(mandatoryChar));
+                        }
+
+                        this.expectedText = text.toString();
+                        this.resourceName = textView.getResources().getResourceEntryName(labelId);
+                    } catch (Resources.NotFoundException e) {
+                        Log.e(LOG_TAG, "MdkViewMatchers.withCharSequence failure", e);
+                    }
+                }
+
+                CharSequence actualText ;
+                if ( labelLocation == LABEL_TEXT ) {
+                    actualText = textView.getText();
+                } else {
+                    actualText = textView.getHint();
+                }
+
+                return null != this.expectedText && null != actualText?this.expectedText.equals(actualText.toString()):false;
+            }
+        };
+    }
+
+    /**
+     * Define the list of possible label locations
+     */
+    @IntDef({LABEL_TEXT, LABEL_HINT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface LabelLocation {
+        // nothint in the annotation interface
+    }
+
+    /**
+     * Label is located in the text of the view.
+     */
+    public static final int LABEL_TEXT = 0;
+
+    /**
+     * Label is located in the hint of the view.
+     */
+    public static final int LABEL_HINT = 1;
+}
