af8f893116ea62781a458b3fc9bd38ad321c4014
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-07-20 13:50:18 +0000

    test lmi

---
+++ core/src/main/java/com/soprasteria/movalysmdk/widget/core/TestLmi.java
@@ -0,0 +1,14 @@
+package com.soprasteria.movalysmdk.widget.core;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import android.support.annotation.IntDef;
+
+@Retention(RetentionPolicy.SOURCE)
+@IntDef({})
+public @interface TestLmi {
+
+    public static int TOTO = 1;
+}
+
