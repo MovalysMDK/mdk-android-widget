b57b9883fa1086e5f12f0428e128d6ac146e48ed
Simon Bernardin <simon.bernardin@soprasteria.com>
2015-08-18 07:40:09 +0000

    Fix sur les tests

--- sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/ValidatorTest.java
+++ sample/src/androidTest/java/com/soprasteria/movalysmdk/widget/sample/ValidatorTest.java
@@ -16,11 +16,14 @@
 package com.soprasteria.movalysmdk.widget.sample;
 
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import com.soprasteria.movalysmdk.espresso.action.SpoonScreenshotAction;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -39,6 +42,8 @@ import static org.hamcrest.Matchers.notNullValue;
 /**
  * Test Custom validator on RichEditText widget.
  */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ValidatorTest {
 
     /**
